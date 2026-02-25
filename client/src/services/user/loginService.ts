949257ae2d8613f25da7a2a0c2cb5107d2ec18bd
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-02-26 00:46:16 +0200

    Fix type mismatch

--- client/src/services/user/loginService.ts
+++ client/src/services/user/loginService.ts
@@ -3,5 +3,17 @@ import { ServerActionResponse } from "@/schemas/actions";
 import { LoginRequestType, LoginResponseType } from "@/schemas/user/login";
 
 export const loginUserService = async (data: LoginRequestType): Promise<ServerActionResponse<LoginResponseType>> => {
-    return loginApi(data)
+    const result = await loginApi(data);
+    
+    if (result.success) {
+        return {
+            success: true,
+            data: result.data,
+        };
+    }
+    
+    return {
+        success: false,
+        message: result.error,
+    };
 }
