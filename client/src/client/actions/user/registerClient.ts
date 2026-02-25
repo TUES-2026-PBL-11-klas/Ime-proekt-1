e83baeb7b8c20b62ad4fa84db73c431b587adcde
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-02-26 00:19:19 +0200

    Toast on registration

--- client/src/client/actions/user/registerClient.ts
+++ client/src/client/actions/user/registerClient.ts
@@ -3,12 +3,16 @@
 import { registerUser } from "@/actions/user/register";
 import { ServerActionResponse } from "@/schemas/actions"
 import { RegisterResponseType } from "@/schemas/user/register"
+import { toast } from "@/components/ui/use-toast"
 
 export const registerUserClient = async (formData: FormData): Promise<ServerActionResponse<RegisterResponseType>> => {
     const result = await registerUser(formData);
 
     if(result.success) {
-        // TODO toast
+        toast({
+            title: "Success!",
+            description: "You successfully registered on Library MS."
+        })
     }
 
     return result
