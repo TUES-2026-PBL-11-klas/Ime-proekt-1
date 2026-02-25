ceedf6f219baa2dff0352d09be7a296d01693f37
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-02-26 00:24:16 +0200

    Login service

---
+++ client/src/services/user/loginService.ts
@@ -0,0 +1,7 @@
+import { loginApi } from "@/external/user/loginApi";
+import { ServerActionResponse } from "@/schemas/actions";
+import { LoginRequestType, LoginResponseType } from "@/schemas/user/login";
+
+export const loginUserService = async (data: LoginRequestType): Promise<ServerActionResponse<LoginResponseType>> => {
+    return loginApi(data)
+}
