f852d775b1c1a3d60e541e1c062a2c90ab24c6ea
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-02-26 18:43:52 +0200

    Type error

--- client/app/admin/books/[id]/edit/page.tsx
+++ client/app/admin/books/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { genres } from "@/data/genres";
+import { genres } from "@/dataadf/genres";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
